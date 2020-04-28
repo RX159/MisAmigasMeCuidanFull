f2100f35e80ecf198065d26170ed51799daeb8e4
fabianaserangelli <fabiana.serangelli@hotmail.com>
2020-04-28 13:21:21 -0500

    Modificaciones al front-end; TODO: posicionamiento y responsividad

---
+++ Front/js/admin.js
@@ -0,0 +1,100 @@
+function openPage(pageName, elmnt, color) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].style.backgroundColor = "";
+	}
+	document.getElementById(pageName).style.display = "block";
+	elmnt.style.backgroundColor = color;
+}
+
+var modal = document.getElementById("admin-modal");
+var modalApprove = document.getElementById("admin-modal-aprobar");
+var modalDeny = document.getElementById("admin-modal-denegar");
+var btnReview = document.getElementById("revisar-button");
+var spanClose = document.getElementsByClassName("close")[0];
+var btnSend = document.getElementById("modal-confirmar-button");
+var btnCancel = document.getElementById("modal-cancelar-button");
+
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.id == 'revisar-button') {
+        modal.style.display = "block";
+        modal.innerHTML = `
+	  	<div class="admin-card">
+			<img src="../images/foto-fabiana.png" alt="Fabiana" style="width:100%">
+			<p>Nombre: Fabiana Serangelli</p>
+			<p>Matrícula: A01281445</p>
+			<p>Carrera o departamento: ITC</p>
+			<p>Rol: Estudiante</p><br>
+			<p><button id="aprobar-button" class="admin-button">Aprobar</button></p>
+			<p><button id="denegar-button" class="admin-button">Denegar</button></p>
+		</div>`;
+    }
+    if (e.target && e.target.id == 'aprobar-button') {
+        modalApprove.style.display = "block";
+        modalApprove.innerHTML = `
+	  	<div class="modal-content">
+	  		<span id=close class="close">&times;</span>
+			<p style="margin: 5px;
+			margin-top:70px;
+			text-align: center;">¿Segura que quieres aprobar el registro de esta cuenta?</p><br>
+			<br>
+			<div style="text-align: center;
+			margin-bottom: 50px;">
+				<button id="cancelar-button" class="button-sub">Cancelar</button>
+				<button id="confirmar-button" class="button-add">Confirmar</button>
+			</div>
+			<br>
+		</div>`;
+    }
+    if (e.target && e.target.id == 'denegar-button') {
+        modalDeny.style.display = "block";
+    }
+    if (e.target && e.target.id == 'close') {
+		modalApprove.style.display = "none";
+    }
+    if (e.target && e.target.id == 'cancelar-button') {
+		modalApprove.style.display = "none";
+    }
+    if (e.target && e.target.id == 'confirmar-button') {
+    	modal.style.display = "none";
+		modalApprove.style.display = "none";
+    }
+  //   if (e.target && e.target.id == btnCancel) {
+		// modalDeny.style.display = "none";
+  //   }
+  //   if (e.target && e.target.id == btnSend) {
+		// modal.style.display = "none";
+		// modalDeny.style.display = "none";
+  //   }
+ });
+
+btnCancel.onclick = function() {
+	modalDeny.style.display = "none";
+}
+
+btnSend.onclick = function() {
+	modal.style.display = "none";
+	modalDeny.style.display = "none";
+}
+
+spanClose.onclick = function() {
+	modalDeny.style.display = "none";
+}
+
+// Se cierra el modal al presionar fuera de este mismo
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none"
+	}
+	if (event.target == modalApprove) {
+		modalApprove.style.display = "none"
+	}
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
--- Front/js/home.js
+++ Front/js/home.js
@@ -1,4 +1,4 @@
-function openPage(pageName,elmnt,color) {
+function openPage(pageName, elmnt, color) {
 	var i, tabcontent, tablinks;
 	tabcontent = document.getElementsByClassName("tabcontent");
 	for (i = 0; i < tabcontent.length; i++) {
@@ -12,62 +12,16 @@ function openPage(pageName,elmnt,color) {
 	elmnt.style.backgroundColor = color;
 }
 
-//Ricky creacion 0
-var token = localStorage.getItem('token');
-if (token) {
-  token = token.replace(/^"(.*)"$/, '$1');
-}
-
-// Fin creacion Ricky
-
 var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
 var spanClose = document.getElementsByClassName("close")[0];
+var spanClose2 = document.getElementsByClassName("close2")[0];
+var spanClose3 = document.getElementsByClassName("close3")[0];
 var btnAdd1 = document.getElementById("plus-button-1");
 var btnAdd2 = document.getElementById("plus-button-2");
-var btnPublish = document.getElementById("publish-button")
-
-//Inicio de Metio Ricky
-//Al cargarse la pagina debe tratar de conseguir todas las tarjetas que ya existen
-
-function loadPosts() {
-  console.log("entro al load")
-  $.ajax({
-    url: 'http://localhost:3000/posts',
-    headers: {
-        'Content-Type':'application/json',
-        'Authorization': 'Bearer ' + token
-    },
-    method: 'GET',
-    dataType: 'json',
-    success: function(data){
-      for( let i = 0; i < data.length; i++) {
-        LoadPosts(data[i]._id, data[i].asunto, data[i].historia)
-      }
-    },
-    error: function(error_msg) {
-      alert((error_msg['responseText']));
-    }
-  });
-}
-
-loadPosts()
-
-
-function LoadPosts(id, asunto, historia) {
-	modal.style.display = "none";
-	var tituloCard = asunto;
-	var descripCard = historia;
-	$('#id-card-1').prepend(
-		`<div class="column">
-			<div class="card">
-	  			<h3>${asunto}</h3>
-	  			<p>${historia}</p>
-	  		</div>
-	  	</div>`);
-
-}
-
-//Fin de Metio Ricky
+var btnPublish = document.getElementById("publish-button");
+var btnPublish2 = document.getElementById("publish-button-2");
 
 // Se abre el modal del Tendedero
 btnAdd1.onclick = function() {
@@ -77,8 +31,6 @@ btnAdd1.onclick = function() {
 // Se publica en el Tendedero después de presionar el botón
 btnPublish.onclick = function() {
 	modal.style.display = "none";
-	var tituloCard = $('#titulo').val();
-	var descripCard = $('#descripcion').val();
 	$('#id-card-1').prepend(
 		`<div class="column">
 			<div class="card">
@@ -86,58 +38,8 @@ btnPublish.onclick = function() {
 	  			<p>${$('#descripcion').val()}</p>
 	  		</div>
 	  	</div>`);
-
-
-// Inico cambio 2 Ricky
-
-	json_to_send = {
-      "asunto": tituloCard,
-      "historia": descripCard
-    }
-
-    json_to_send = JSON.stringify(json_to_send);
-
-    $.ajax({
-       url: 'http://localhost:3000/posts',
-      //url: 'https://miniwebserverrx.herokuapp.com/users',
-      headers: {
-          'Content-Type':'application/json',
-        'Authorization': 'Bearer ' + token
-      },
-      method: 'POST',
-      dataType: 'json',
-      data: json_to_send,
-      success: function(data){
-        alert("Successfully created");
-      },
-      error: function(error_msg) {
-        if($id.val() != '') {
-          console.log(error_msg)
-          $id2_empty.removeClass('hidden')
-        }
-      }
-    })
-// Fin cambio 2 Ricky, Me mamas Javi
-
 	document.getElementById('titulo').value = '';
 	document.getElementById('descripcion').value = '';
-
-}
-
-// Se publica en el Acompañar
-btnAdd2.onclick = function() {
-	$('#id-card-2').prepend(
-		`<div class="column">
-			<div class="card">
-	  			<h3>Título con hora y lugar</h3>
-	  			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	  	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	  	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	  	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	  	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	  	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-	  		</div>
-	  	</div>`);
 }
 
 // Se cierra el modal al presionar la x
@@ -145,12 +47,114 @@ spanClose.onclick = function() {
 	modal.style.display = "none";
 }
 
+// Se publica en el Acompañar
+btnAdd2.onclick = function() {
+	modal2.style.display = "block";
+}
+
+// Se publica en Acompañar después de presionar el botón
+btnPublish2.onclick = function() {
+	modal2.style.display = "none";
+	$('#id-card-2').prepend(
+		`<div class="column">
+			<div class="card">
+	  			<h3>${$('#lugar').val()}</h3>
+	  			<h5 style="border-style: solid;
+ 					border-color: #ccccff;
+ 					border-width: thin;
+ 					padding: 5px;
+ 					margin-bottom: -1px">Fecha: ${$('#fecha').val()}</h3>
+	  			<h5 style="border-style: solid;
+ 					border-color: #ccccff;
+ 					border-width: thin;
+ 					padding: 5px;
+ 					margin-top: 0px;
+ 					margin-bottom: -1px;">Hora inicio: ${$('#hora-inicio').val()}</h3>
+	  			<h5 style="border-style: solid;
+ 					border-color: #ccccff;
+ 					border-width: thin;
+ 					padding: 5px;
+ 					margin-top: -0.5px">Hora fin: ${$('#hora-fin').val()}</h3>
+	  			
+	  			<i id="detalles-button" class="fa fa-ellipsis-h tooltip" aria-hidden="true">
+	  				<span class="tooltiptext">Presiona para leer todos los detalles</span>
+	  			</i><br>
+	  			<br>
+	  			<button id="acompanar-button" class="button-add">Acompañar</button>
+	  		</div>
+	  	</div>`);
+	document.getElementById('lugar').value = '';
+	document.getElementById('hora-inicio').value = '';
+	document.getElementById('hora-fin').value = '';
+	document.getElementById('descripcion2').value = '';
+	document.getElementById('fecha').value = '';
+}
+
+document.addEventListener('click', function(e) {
+    if (e.target && e.target.id == 'detalles-button') {
+          modal3.style.display = "block";
+          modal3.innerHTML = `
+	  		<div class="modal-content">
+				<span id= close class="close3">&times;</span>
+				<p style="margin: 5px;
+				margin-top:30px;">Lugar: </p><br>
+				<br>
+				<p style="margin: 5px;">Fecha: </p><br>
+				<br>
+				<p style="margin: 5px;">Hora inicio: </p><br>
+				<br>
+				<p style="margin: 5px;">Hora fin: </p><br>
+				<br >
+				<p style="margin: 5px;">Descripción: </p><br>
+		    </div>
+	  	</div>`;
+    }
+    if (e.target && e.target.id == 'acompanar-button') {
+          modal3.style.display = "block";
+          modal3.innerHTML = `
+	  		<div class="modal-content">
+				<p style="margin: 5px;
+				margin-top:70px;
+				text-align: center;">¿Segura que quieres confirmar asistir a este lugar a la hora indicada?</p><br>
+				<br>
+				<div style="text-align: center;
+				margin-bottom: 50px;">
+					<button id="cancelar-button" class="button-sub">Cancelar</button>
+					<button id="confirmar-button" class="button-add">Confirmar</button>
+				</div>
+				<br>
+		    </div>
+	  	</div>`;
+    }
+    if (e.target && e.target.id == 'close') {
+		modal3.style.display = "none";
+    }
+    if (e.target && e.target.id == 'cancelar-button') {
+		modal3.style.display = "none";
+    }
+    if (e.target && e.target.id == 'confirmar-button') {
+		modal3.style.display = "none";
+    }
+ });
+
+// Se cierra el modal al presionar la x
+spanClose2.onclick = function() {
+	modal2.style.display = "none";
+}
+
 // Se cierra el modal al presionar fuera de este mismo
 window.onclick = function(event) {
 	if (event.target == modal) {
 		modal.style.display = "none"
 	}
+	if (event.target == modal2) {
+		modal2.style.display = "none"
+	}
+	if (event.target == modal3) {
+		modal3.style.display = "none"
+	}
 }
+
 
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
